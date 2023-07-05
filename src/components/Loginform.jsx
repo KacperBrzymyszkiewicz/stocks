be518e1d62dfb3448144ac6b4ff1963e17118be1
Kacper Brzymyszkiewicz <brzymyszkiewiczkacper@gmail.com>
2023-07-05 17:55:26 +0200

    s

--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -13,8 +13,8 @@ function Loginform(props){
             <div id='marg'>
             <h1>Sign in to join</h1>
             <h2>Login and check your portfolio</h2>
-            <button className='sin-btn'><FontAwesomeIcon icon={faGoogle} size='xl'/> Sign in with Google</button>
-            <button className='sin-btn'><FontAwesomeIcon icon={faFacebook} size='xl'/> Sign in with Facebook</button>
+            <button className='sin-btn'><FontAwesomeIcon className='icon-btn' icon={faGoogle} size='2xl'/> Sign in with Google</button>
+            <button className='sin-btn'><FontAwesomeIcon className='icon-btn' icon={faFacebook} size='2xl'/> Sign in with Facebook</button>
             <div id='divide'>
             <hr/><h2>or</h2><hr />
             </div>
