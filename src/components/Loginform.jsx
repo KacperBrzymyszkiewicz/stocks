b248195832306f85009ed30de692d05a045cc965
Kacper Brzymyszkiewicz <brzymyszkiewiczkacper@gmail.com>
2023-07-05 17:00:14 +0200

    k

--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import  ReactDOM  from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons'
 import '../styles/login.css'
 function Loginform(props){
     if(props.login == true){
@@ -11,8 +13,8 @@ function Loginform(props){
             <div id='marg'>
             <h1>Sign in to join</h1>
             <h2>Login and check your portfolio</h2>
-            <button className='sin-btn'>Sign in with Google</button>
-            <button className='sin-btn'>Sign in with Facebook</button>
+            <button className='sin-btn'><FontAwesomeIcon icon={faGoogle} size='xl'/> Sign in with Google</button>
+            <button className='sin-btn'><FontAwesomeIcon icon={faFacebook} size='xl'/> Sign in with Facebook</button>
             <div id='divide'>
             <hr/><h2>or</h2><hr />
             </div>
